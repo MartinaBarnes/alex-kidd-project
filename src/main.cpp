ebf527fb664473bf295a8f4b0ac4c33febed3e06
alberto-blanco-celdran <albertoblancoceldran@gmail.com>
2026-04-15 18:25:11 +0200

    Added screen scaling

--- src/main.cpp
+++ src/main.cpp
@@ -11,39 +11,44 @@
 #include "Scene.h"
 #include <cmath>
 
+#define GAME_WIDTH 256
+#define GAME_HEIGHT 192
+
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI);
+	InitWindow(GAME_WIDTH, GAME_HEIGHT, "Alex Kidd in Miracle World");
+	SetTargetFPS(60);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Alex Kidd in Miracle World");
+	RenderTexture2D target = LoadRenderTexture(GAME_WIDTH, GAME_HEIGHT);
+	Rectangle targetSrc = Rectangle { 0, 0, GAME_WIDTH, -GAME_HEIGHT };
+	Rectangle targetDest = Rectangle { 0 };
+
+	AABB* bounds = new AABB(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
 	ResourceManager::loadResources();
 
-	AABB* bounds = new AABB(0, 0, 1280, 800);
-
 	TileMap* tilemap = new TileMap();
-	tilemap->physics->map[1][31] = PHYSTILE_SOLID;
-	tilemap->physics->map[16][31] = PHYSTILE_SOLID;
-	tilemap->physics->map[16][30] = PHYSTILE_SOLID;
-	tilemap->physics->map[24][31] = PHYSTILE_SOLID;
-	for (int i = 0; i < 128; i++) {
-		tilemap->physics->map[i][32] = PHYSTILE_SOLID;
+	tilemap->physics->map[1][9] = PHYSTILE_SOLID;
+	tilemap->physics->map[5][9] = PHYSTILE_SOLID;
+	tilemap->physics->map[5][8] = PHYSTILE_SOLID;
+	tilemap->physics->map[12][9] = PHYSTILE_SOLID;
+	for (int i = 0; i < 32; i++) {
+		tilemap->physics->map[i][10] = PHYSTILE_SOLID;
 	}
 	tilemap->render->texture = ResourceManager::getTexture("tiles");
 	tilemap->render->tiles[0] = Rectangle { 48, 0, 16, 16 };
 	tilemap->render->tiles[1] = Rectangle { 48, 32, 16, 16 };
-	tilemap->render->map[1][31] = 1;
-	tilemap->render->map[16][31] = 2;
-	tilemap->render->map[16][30] = 1;
-	tilemap->render->map[24][31] = 1;
+	tilemap->render->map[1][9] = 1;
+	tilemap->render->map[5][9] = 2;
+	tilemap->render->map[5][8] = 1;
+	tilemap->render->map[12][9] = 1;
 	for (int i = 0; i < 128; i++) {
-		tilemap->render->map[i][32] = 1;
-		if (i == 1 || i == 16 || i == 24) {
-		    tilemap->render->map[i][32] = 2;
+		tilemap->render->map[i][10] = 1;
+		if (i == 1 || i == 5 || i == 12) {
+		    tilemap->render->map[i][10] = 2;
 		}
-		tilemap->render->map[i][33] = 2;
+		tilemap->render->map[i][11] = 2;
 	}
 	tilemap->render->bounds = bounds;
 
@@ -51,8 +56,8 @@ int main ()
 	WanderingEnemy* enemy = new WanderingEnemy();
 	enemy->walk_speed = 100.0f;
 	enemy->physics->aabb.size = { 16, 16 };
-	enemy->physics->aabb.position.x = 16 * TILE_SIZE;
-	enemy->physics->aabb.position.y = 31 * TILE_SIZE;
+	enemy->physics->aabb.position.x = 6 * TILE_SIZE;
+	enemy->physics->aabb.position.y = 9 * TILE_SIZE;
 	enemy->sprite->animation = new Animation();
 	enemy->sprite->animation->texture = ResourceManager::getTexture("wabbit_alpha");
 	enemy->sprite->animation->frames.push_back(Rectangle { 0, 0, 32, 32 });
@@ -62,29 +67,34 @@ int main ()
 	scene->push(player);
 	scene->push(enemy);
 
-	// game loop
 	while (!WindowShouldClose()) // run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
+	    int scnW = GetScreenWidth();
+		int scnH = GetScreenHeight();
+	    int scale = (scnW > scnH) ? scnW / GAME_WIDTH : scnH / GAME_HEIGHT;
+
+		targetDest.width = GAME_WIDTH * scale;
+		targetDest.height = GAME_HEIGHT * scale;
+		targetDest.x = scnW / 2 - targetDest.width / 2;
+		targetDest.y = scnH / 2 - targetDest.height / 2;
+
 	    float dt = GetFrameTime();
         scene->update(dt);
 		PhysicsServer::update(dt, bounds);
 
-		// drawing
-		BeginDrawing();
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
+		BeginTextureMode(target);
 		ClearBackground(BLUE);
-
 		RenderingServer::update(dt, bounds);
+		EndTextureMode();
 
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawTexturePro(target.texture, targetSrc, targetDest, Vector2 { 0, 0 }, 0.0f, WHITE);
 		EndDrawing();
 	}
 
-	// cleanup
 	ResourceManager::unloadResources();
 
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
