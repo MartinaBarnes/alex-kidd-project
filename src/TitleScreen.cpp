3530f16d3b16fe2f235bf9abe134911547f7b45e
alberto-blanco-celdran <albertoblancoceldran@gmail.com>
2026-04-20 14:31:57 +0200

    Title screen is skipped with a number of buttons

--- src/TitleScreen.cpp
+++ src/TitleScreen.cpp
@@ -3,7 +3,7 @@
 #include "SceneFactory.h"
 
 void TitleScreen::update(float _) {
-    if (IsKeyPressed(0)) {
+    if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_DOWN)) {
         SceneManager::replace(SceneFactory::intro1());
         return;
     }
